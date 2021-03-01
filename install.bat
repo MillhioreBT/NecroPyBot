f39846925d7ca2b652128123eab11961046872f9
MillhioreBT <sarahelizabetwesker@gmail.com>
2021-03-01 03:30:40 -0400

    correct order in install

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
-move base_wrapper.py %localappdata%\Programs\Python\Python38-32\Lib\site-packages\pywinauto\
-move hwndwrapper.py %localappdata%\Programs\Python\Python38-32\Lib\site-packages\pywinauto\controls\
 pip install -r requirements.txt
 pip install requests
+move base_wrapper.py %localappdata%\Programs\Python\Python38-32\Lib\site-packages\pywinauto\
+move hwndwrapper.py %localappdata%\Programs\Python\Python38-32\Lib\site-packages\pywinauto\controls\
 del install.bat
